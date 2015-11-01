2f845959b5677c537682f16c220bd62771723417
Li Lok Hei <s0609674@gmail.com>
2015-11-01 21:55:08 +0800

    Add Logger

---
+++ src/simulator/Logger.java
@@ -0,0 +1,35 @@
+package simulator;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Logger {
+  
+  private static String fileName;
+  static File log;
+  
+  public Logger(String fn) {
+    fileName = fn;
+    
+  }
+  
+  public static void createLog(String msg) {
+    System.out.println(msg);
+    
+    log = new File(fileName+".txt");
+    try{
+      if(log.exists()==false){
+              log.createNewFile();
+      }
+      PrintWriter out = new PrintWriter(new FileWriter(log, true));
+      out.append(msg + System.getProperty("line.separator"));
+      out.close();
+      }catch(IOException e){
+          System.out.println("COULD NOT LOG!!");
+      }
+    
+  }
+    
+}
